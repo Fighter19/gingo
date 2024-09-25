a206c2beb0cc59dc43d170144cd5b18fc66f7597
Fighter19 <1475802+Fighter19@users.noreply.github.com>
2024-09-25 15:45:11 +0200

    Initialize interface mostly correctly, some bug is still left

--- include/glib-object.h
+++ include/glib-object.h
@@ -40,12 +40,17 @@ typedef struct _GTypeInfo {
 	gpointer *_unused;
 } GTypeInfo;
 
-/** Contains information about to (de-)initialize an interface. */
+/** Contains information about to (de-)initialize an interface. 
+ * 
+ * This is used to initialize the vtable of an interface.
+*/
 typedef struct _GInterfaceInfo {
+  // Called when the vtable of the interface is being constructed
 	GInterfaceInitFunc interface_init_func;
+  // Called when the vtable of the interface is being destroyed
 	GInterfaceFinalizeFunc interface_finalize_func;
-	void *_unused;
-	void *_unused2;
+  // User data that is passed to the init and finalize functions
+	void *user_data;
 } GInterfaceInfo;
 
 /*
@@ -55,11 +60,25 @@ typedef struct _GInterfaceInfo {
 
 typedef int GType;
 
+typedef struct _InterfaceEntry {
+  /* Type of the interface implemented by the class */
+  GType *type;
+  /* Information on how to construct the vtable of the interface */
+  GInterfaceInfo *info;
+  /* Virtual function table of the interface implemented by the class */
+  gpointer vtable;
+  /* Size of virtual function table */
+  gsize vtable_size;
+} InterfaceEntry;
+
+#define MAX_INTERFACES_PER_TYPE 4
 /** Cotains info about class hierarchy */
 typedef struct _GTypeClass {
 	GType type;
 	struct _GTypeClass *parent;
-	const GInterfaceInfo *info;
+  // Interfaces that the class implements
+  InterfaceEntry interfaces[MAX_INTERFACES_PER_TYPE];
+  gsize n_interfaces;
 } GTypeClass;
 
 /** Contains the type of an instance of a class. */
@@ -71,6 +90,7 @@ typedef struct _GTypeInstance {
 typedef struct _GTypeInterface {
 	GType type;
 	struct _GTypeInterface *parent;
+  const GInterfaceInfo *info;
 } GTypeInterface;
 
 /** Contains information about a type that specifies an object. */
@@ -91,6 +111,12 @@ typedef struct _GObject {
 static void *g_malloc(gsize count)
 {
 	return malloc(count);
+}
+
+/** Called for tracking memory allocations with static lifetime. */
+static void *g_malloc_static(gsize count)
+{
+  return g_malloc(count);
 }
 
 static void g_free(void *ptr)
@@ -129,10 +155,17 @@ static GType g_type_register_static(GType parent, const gchar *type_name,
 	return g_typePool.last++;
 }
 
-/** Adds an interface to a class. */
+/** Adds an interface to a class.
+ * @param instance_type The type of the class implementing the interface.
+ * @param interface_type The type of the interface.
+ * @param info Function pointer table used to initialize the vtable of the interface.
+ */
 static void g_type_add_interface_static(GType instance_type, GType interface_type, const GInterfaceInfo *info)
 {
-	g_typePool.classes[instance_type].info = info;
+  InterfaceEntry entry = { .type = &g_typePool.classes[interface_type].type, .info = (GInterfaceInfo*)info };
+  gsize n_interfaces = g_typePool.classes[instance_type].n_interfaces;
+  g_typePool.classes[instance_type].interfaces[n_interfaces] = entry;
+	g_typePool.classes[instance_type].n_interfaces++;
 }
 
 #define g_type_class_peek_parent(klass) g_type_class_peek_parent_actual((GTypeClass*)klass)
@@ -144,10 +177,13 @@ static GTypeClass *g_type_class_peek_parent_actual(GTypeClass *klass)
 #define g_type_interface_peek_parent(interface) (gpointer)g_type_interface_peek_parent_actual((GTypeInterface*)interface)
 static GTypeInterface *g_type_interface_peek_parent_actual(GTypeInterface *interface)
 {
+  if (interface == NULL) {
+    return NULL;
+  }
 	return interface->parent;
 }
 
-static void g_type_init()
+__attribute__((constructor)) static void g_type_init()
 {
 	g_assert(g_typePool.last == 0);
 	static const GTypeInfo g_type_class_type_info = {
@@ -181,7 +217,7 @@ static void g_type_init()
 		.class_init_func = 0,
 		.class_finalize_func = 0,
 		.class_data = 0,
-		.object_size = sizeof(GObject),
+		.object_size = 0,
 		.preallocs = 0,
 		.instance_init = 0,
 		._unused = 0
@@ -196,14 +232,36 @@ static void g_type_init()
 	g_typePool.classes[G_TYPE_INTERFACE].type = G_TYPE_INTERFACE;
 
 	g_typePool.classes[G_TYPE_TYPE].parent = NULL;
-	g_typePool.classes[G_TYPE_OBJECT].parent = G_TYPE_TYPE;
+	g_typePool.classes[G_TYPE_OBJECT].parent = &g_typePool.classes[G_TYPE_TYPE];
 	g_typePool.classes[G_TYPE_INTERFACE].parent = NULL;
 
 	g_typePool.last = G_TYPE_BUILTIN_COUNT;
 }
 
-static gpointer private_g_type_get_interface(GType type)
+static GTypeClass *g_type_get_class(GType type)
 {
+  return &g_typePool.classes[type];
+}
+
+static gpointer private_g_type_get_interface(GTypeInstance *obj, GType type, gsize vtable_size)
+{
+  GTypeClass *klass = g_type_get_class(obj->type);
+  for (gsize i = 0; i < klass->n_interfaces; i++) {
+    if (*klass->interfaces[i].type == type) {
+      InterfaceEntry *entry = &klass->interfaces[i];
+      entry->vtable_size = vtable_size;
+      // Lazily initialize the vtable (I haven't found another way to obtain the vtable size)
+      if (entry->vtable == NULL) {
+        entry->vtable = g_malloc_static(vtable_size);  
+        entry->info->interface_init_func(
+          entry->vtable, 
+          entry->info->user_data
+        );
+      }
+      return (gpointer)klass->interfaces[i].vtable;
+    }
+  }
+  return NULL;
 }
 
 // According to the doc, a no-op is acceptable
@@ -212,7 +270,7 @@ static gpointer private_g_type_get_interface(GType type)
 #define g_return_val_if_fail(cond, val) \
 	if (!(cond))                    \
 		return val;
-#define G_TYPE_INSTANCE_GET_INTERFACE(obj, type_id, interface_name) private_g_type_get_interface(type_id)
+#define G_TYPE_INSTANCE_GET_INTERFACE(obj, type_id, interface_name) private_g_type_get_interface((GTypeInstance*)obj, type_id, sizeof(interface_name))
 
 // These functions are probably implemented in a thread-safe way in a multi-threaded environment
 static bool g_once_init_enter(volatile gsize *location)
@@ -251,6 +309,8 @@ static void g_object_unref_actual(GObject *obj)
 	}
 }
 
+// Originally, this was used internally to check if the instance is of a fundamental type
+// However this was added before realizing that a GObject likely derives from GTypeInstance
 static bool
 g_type_check_instance_is_fundamentally_a(GTypeClass *instance, GType fundamental_type)
 {
@@ -271,6 +331,18 @@ g_type_check_instance_is_fundamentally_a(GTypeClass *instance, GType fundamental
 	return false;
 }
 
+static bool
+g_type_check_instance_is_a(GTypeInstance *instance, GType type)
+{
+  // Iterate through the class hierarchy
+  GTypeClass *klass;
+  for (klass = g_type_get_class(instance->type); klass; klass = g_type_class_peek_parent(klass)) {
+    if (klass->type == type) {
+      return true;
+    }
+  }
+}
+
 static void* g_object_new(GType type, const gchar *first_property_name, ...)
 {
 	// g_type_init() should be called before this function
@@ -278,10 +350,10 @@ static void* g_object_new(GType type, const gchar *first_property_name, ...)
 	GTypeClass *klass = &g_typePool.classes[type];
 	GObject *obj = g_malloc(g_typePool.pool[type].object_size);
 	g_assert(obj);
-	if (g_type_check_instance_is_fundamentally_a(klass, G_TYPE_TYPE)) {
+	if (g_type_check_instance_is_a(klass, G_TYPE_TYPE)) {
 		obj->base.type = type;
 	}
-	if (g_type_check_instance_is_fundamentally_a(klass, G_TYPE_OBJECT)) {
+	if (g_type_check_instance_is_a(klass, G_TYPE_OBJECT)) {
 		obj->ref_count = 1;
 	}
 
